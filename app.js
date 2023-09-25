ddc344460f989e94a371231ba105567ef8de3363
26june <gj.abordo1@gmail.com>
2023-09-25 15:26:06 +0100

    add comment

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ const seattle = {
           this.minCustomer
       );
 
+      //cant have 0.2 of cookie so round up (or should it go down? idk)
       let cookiesSold = Math.ceil(randomCustomer * this.avgCookiesPerCustomer);
 
       this.cookieSales[i] = `${serviceHours[i]}: ${cookiesSold}`;
@@ -61,3 +62,53 @@ const seattle = {
 
 seattle.randomNumGen();
 seattle.displayData();
+
+const tokyo = {
+  location: "Tokyo",
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookiesPerCustomer: 1.2,
+  cookieSales: [],
+  totalSales: 0,
+
+  randomNumGen: function () {
+    //Generate random number of customers
+    for (let i = 0; i < serviceHours.length; i++) {
+      let randomCustomer = Math.floor(
+        Math.random() * (this.maxCustomer - this.minCustomer + 1) +
+          this.minCustomer
+      );
+
+      //cant have 0.2 of cookie so round up (or should it go down? idk)
+      let cookiesSold = Math.ceil(randomCustomer * this.avgCookiesPerCustomer);
+
+      //
+      this.cookieSales[i] = `${serviceHours[i]}: ${cookiesSold}`;
+
+      this.totalSales += cookiesSold;
+    }
+
+    this.cookieSales.push(`Total: ${this.totalSales}`);
+
+    return this.cookieSales;
+  },
+
+  displayData: function () {
+    let salesData = document.getElementById("salesData");
+    let h2 = document.createElement("h2");
+    h2.textContent = this.location;
+    salesData.appendChild(h2);
+
+    let ul = document.createElement("ul");
+    salesData.appendChild(ul);
+
+    for (let i = 0; i < this.cookieSales.length; i++) {
+      let li = document.createElement("li");
+      li.textContent = this.cookieSales[i];
+      ul.appendChild(li);
+    }
+  },
+};
+
+tokyo.randomNumGen();
+tokyo.displayData();
