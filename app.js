efb2605b0b0e52fd8cf024d52bc871252371fae9
26june <gj.abordo1@gmail.com>
2023-09-25 15:54:39 +0100

    add other location

--- app.js
+++ app.js
@@ -112,3 +112,153 @@ const tokyo = {
 
 tokyo.randomNumGen();
 tokyo.displayData();
+
+const dubai = {
+  location: "Dubai",
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgCookiesPerCustomer: 3.7,
+  cookieSales: [],
+  totalSales: 0,
+
+  randomNumGen: function () {
+    //Generate random number of customers
+    for (let i = 0; i < serviceHours.length; i++) {
+      let randomCustomer = Math.floor(
+        Math.random() * (this.maxCustomer - this.minCustomer + 1) +
+          this.minCustomer
+      );
+
+      //cant have 0.2 of cookie so round up (or should it go down? idk)
+      let cookiesSold = Math.ceil(randomCustomer * this.avgCookiesPerCustomer);
+
+      //
+      this.cookieSales[i] = `${serviceHours[i]}: ${cookiesSold}`;
+
+      this.totalSales += cookiesSold;
+    }
+
+    this.cookieSales.push(`Total: ${this.totalSales}`);
+
+    return this.cookieSales;
+  },
+
+  displayData: function () {
+    let salesData = document.getElementById("salesData");
+    let h2 = document.createElement("h2");
+    h2.textContent = this.location;
+    salesData.appendChild(h2);
+
+    let ul = document.createElement("ul");
+    salesData.appendChild(ul);
+
+    for (let i = 0; i < this.cookieSales.length; i++) {
+      let li = document.createElement("li");
+      li.textContent = `${this.cookieSales[i]} cookies`;
+      ul.appendChild(li);
+    }
+  },
+};
+
+dubai.randomNumGen();
+dubai.displayData();
+
+const paris = {
+  location: "Paris",
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgCookiesPerCustomer: 2.3,
+  cookieSales: [],
+  totalSales: 0,
+
+  randomNumGen: function () {
+    //Generate random number of customers
+    for (let i = 0; i < serviceHours.length; i++) {
+      let randomCustomer = Math.floor(
+        Math.random() * (this.maxCustomer - this.minCustomer + 1) +
+          this.minCustomer
+      );
+
+      //cant have 0.2 of cookie so round up (or should it go down? idk)
+      let cookiesSold = Math.ceil(randomCustomer * this.avgCookiesPerCustomer);
+
+      //
+      this.cookieSales[i] = `${serviceHours[i]}: ${cookiesSold}`;
+
+      this.totalSales += cookiesSold;
+    }
+
+    this.cookieSales.push(`Total: ${this.totalSales}`);
+
+    return this.cookieSales;
+  },
+
+  displayData: function () {
+    let salesData = document.getElementById("salesData");
+    let h2 = document.createElement("h2");
+    h2.textContent = this.location;
+    salesData.appendChild(h2);
+
+    let ul = document.createElement("ul");
+    salesData.appendChild(ul);
+
+    for (let i = 0; i < this.cookieSales.length; i++) {
+      let li = document.createElement("li");
+      li.textContent = `${this.cookieSales[i]} cookies`;
+      ul.appendChild(li);
+    }
+  },
+};
+
+paris.randomNumGen();
+paris.displayData();
+
+const lima = {
+  location: "Lima",
+  minCustomer: 2,
+  maxCustomer: 16,
+  avgCookiesPerCustomer: 4.6,
+  cookieSales: [],
+  totalSales: 0,
+
+  randomNumGen: function () {
+    //Generate random number of customers
+    for (let i = 0; i < serviceHours.length; i++) {
+      let randomCustomer = Math.floor(
+        Math.random() * (this.maxCustomer - this.minCustomer + 1) +
+          this.minCustomer
+      );
+
+      //cant have 0.2 of cookie so round up (or should it go down? idk)
+      let cookiesSold = Math.ceil(randomCustomer * this.avgCookiesPerCustomer);
+
+      //
+      this.cookieSales[i] = `${serviceHours[i]}: ${cookiesSold}`;
+
+      this.totalSales += cookiesSold;
+    }
+
+    this.cookieSales.push(`Total: ${this.totalSales}`);
+
+    return this.cookieSales;
+  },
+
+  displayData: function () {
+    let salesData = document.getElementById("salesData");
+    let h2 = document.createElement("h2");
+    h2.textContent = this.location;
+    salesData.appendChild(h2);
+
+    let ul = document.createElement("ul");
+    salesData.appendChild(ul);
+
+    for (let i = 0; i < this.cookieSales.length; i++) {
+      let li = document.createElement("li");
+      li.textContent = `${this.cookieSales[i]} cookies`;
+      ul.appendChild(li);
+    }
+  },
+};
+
+lima.randomNumGen();
+lima.displayData();
